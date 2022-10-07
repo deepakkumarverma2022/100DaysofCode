9c993dcfd08d8c6d72e98e2c225416f854b8529f
deepakkumarverma2022 <115134235+deepakkumarverma2022@users.noreply.github.com>
2022-10-07 23:47:34 +0530

    Update java stdin stdout.java

--- day2/java stdin stdout.java
+++ day2/java stdin stdout.java
@@ -1,1 +1,33 @@
+I simply have taken three integer input & printed it.
+ Sample Input: 
+42
+100
+125
+Sample Output
+
+42
+100
+125
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
 

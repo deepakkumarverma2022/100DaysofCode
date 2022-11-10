e60efce23874a08d3ec4190fe16780f2c5d90c81
deepakkumarverma2022 <115134235+deepakkumarverma2022@users.noreply.github.com>
2022-11-10 20:09:23 +0530

    Create Stdin_Stdout2.java

---
+++ Day10/Stdin_Stdout2.java
@@ -0,0 +1,35 @@
+/*
+WAP to take input as integer, double & String from the user and print them as it is said in the question.
+
+
+Sample Input
+
+42
+3.1415
+Welcome to HackerRank's Java tutorials!
+Sample Output
+
+String: Welcome to HackerRank's Java tutorials!
+Double: 3.1415
+Int: 42
+*/
+
+
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+      
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+      
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}

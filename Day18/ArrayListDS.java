56ed449f5adcbdde4157654ef72cbac3cd6ce9bc
Deepak Verma <115134235+deepakkumarverma2022@users.noreply.github.com>
2022-11-20 23:35:20 +0530

    Create ArrayListDS.java

---
+++ Day18/ArrayListDS.java
@@ -0,0 +1,77 @@
+/*
+An array is a type of data structure that stores elements of the same type in a contiguous block of memory.
+In an array,A, of size N, each memory location has some unique index,i that can be referenced as A[i].
+
+Reverse an array of integers.
+
+Sample Input 1
+
+Array: arr
+1 4 3 2
+
+Sample Output 1
+
+2 3 4 1
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+    ArrayList<Integer> a1 = new ArrayList<>();
+    for(int i = a.size()-1; i >=0; i--) {
+        a1.add(a.get(i));
+    }
+        return a1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> res = Result.reverseArray(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            bufferedWriter.write(String.valueOf(res.get(i)));
+
+            if (i != res.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

7d107127090d181c931d22dfa882053665e655d7
Deepak Verma <115134235+deepakkumarverma2022@users.noreply.github.com>
2023-03-31 16:06:56 +0530

    Create Leaders.java

---
+++ Day43/Leaders.java
@@ -0,0 +1,56 @@
+/*
+In this program given an array A of positive integers. 
+Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. 
+The rightmost element is always a leader. 
+
+Example 1:
+
+Input:
+n = 6
+A[] = {16,17,4,3,5,2}
+Output: 17 5 2
+Explanation: The first leader is 17 
+as it is greater than all the elements
+to its right.  Similarly, the next 
+leader is 5. The right most element 
+is always a leader so it is also 
+included.
+ 
+
+Example 2:
+
+Input:
+n = 5
+A[] = {1,2,3,4,0}
+Output: 4 0
+ 
+
+Your Task:
+You don't need to read input or print anything. 
+The task is to complete the function leader() which takes array A and n as input parameters and returns an array of leaders in order of their appearance.
+
+ 
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+*/
+
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        ArrayList <Integer> number = new ArrayList<>();
+        
+        int curr_elem = arr[n-1];
+        for(int i = n-1; i >= 0; i--) {
+            if(arr[i] >= curr_elem) {
+                number.add(arr[i]);
+                curr_elem = arr[i];
+            }
+        }
+        Collections.reverse(number);
+        return number;
+    }
+}
